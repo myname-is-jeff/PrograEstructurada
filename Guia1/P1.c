1f46199cc622e1da416d60732b84dd39cd56baba
myname-is-jeff <58615693+myname-is-jeff@users.noreply.github.com>
2023-03-17 19:07:58 -0300

    se hace G1P1

--- Guia1/P1.c
+++ Guia1/P1.c
@@ -13,41 +13,50 @@ el programa principal.*/
 #include <stdio.h>
 #include <stdlib.h>
 
-int techo(float num){
-	int x;
-	x = x + num;
-	return x++;
+int techo(double num){
+	int i;
+	
+	for (i = 0; num > 1; i++){
+		num -= 1.0;
+	}
+	i++;
+	return i;
 }
 int piso(float num){
 	int x;
+	
+	x=0;
 	x = x + num;
 	return x;	
 }
 int redondeo(float num){
 	int x;
-	x = x + num;
-	num = num - x;
-	if (num >= 0.5){
-		x = techo(num + x);
+	float dec;
+	
+	x = num;
+	dec = num - x;
+	if (dec >= 0.5){
+		x++;
 	}
 	return x;
 }
 
 int main(void){
 	int redon;
-	float num;
+	double num;
 	int t;
 	int p;
-	char c;
 	
 	printf("%s\n","Ingrese un numero racional");
-	scanf("%d\n",num);
-	while((c = getchar()) != '\n' && c != EOF);
-	redon = redondeo(num);
+	scanf("%lf",&num);
+	
 	t = techo(num);
+	redon = redondeo(num);
 	p = piso(num);
+	
 	printf("%s%d\n", "El techo es: ",t);
 	printf("%s%d\n", "El piso es: ",p);
 	printf("%s%d\n", "El numero redondeado es: ",redon);
+	
 	return 0;
 }
