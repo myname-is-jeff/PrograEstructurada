b5d7491e0cd06012b8d013732ebc007aaa12f105
myname-is-jeff <58615693+myname-is-jeff@users.noreply.github.com>
2023-03-21 14:32:14 -0300

    cosas varias

--- Guia1/G1P4.c
+++ Guia1/G1P4.c
@@ -10,5 +10,53 @@ números enteros de tamaño T. Uno de los arreglos deberá ser cargado, luego inver
 por último impreso. El otro arreglo deberá ser cargado, luego ordenado y por último
 impreso. Se debe utilizar las funciones que se solicita desarrollar.
 */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
 
 
+
+void cargarArr(int arr[]){
+	int i;
+	for(i = 0;i != 10 ;i++){
+		arr[i] = (rand() % 10);
+		printf("%d",arr[i]);
+	}
+	
+}
+
+void imprimirArr(int arr[]){
+	size_t len;
+	int i;
+	len = (sizeof(arr)/sizeof(arr[0]));
+	printf("%d\n",((int)len));
+	for(i = 0; i != ((int)len); i++){
+		printf(" ___ ");
+	}
+	printf("\n| ");
+	for(i = 0; i != ((int)len); i++){
+		printf("%d %s",arr[i],"| ");
+	}
+	printf("\n");
+	for(i = 0; i != ((int)len); i++){
+		printf(" --- ");
+	}
+}
+
+void invertirArr(int arr[]){
+	
+}
+
+void ordenarArr(int arr[]){
+	
+}
+
+
+int main(void){
+	int arr[11];
+	
+	cargarArr(arr);
+	imprimirArr(arr);
+	return 0;
+}
