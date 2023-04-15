fa0357e53c0966d050f0c959b4d3eab2ae095466
myname-is-jeff <jolivera@uca.edu.ar>
2023-04-15 10:31:39 -0300

    init G2P1

---
+++ Guia2/G2P1.c
@@ -0,0 +1,45 @@
+/*
+ Crear una estructura que permita guardar información sobre un punto en el plano
+(utilizando sus coordenadas en los ejes x e y). Luego realizar un programa que solicite al
+usuario que se ingresen los datos de dos puntos e indique cuál de los dos puntos se
+encuentra más lejos del origen de coordenadas (0;0)
+*/ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_punto{
+    int x;
+    int y;
+    double norma;
+    char nombre;
+}t_pos;
+
+double norm(int x, int y);
+
+int main(void){
+    t_pos A,B;
+    
+    A.nombre = "A";
+    A.x = 3;
+    A.y = 5;
+    A.norma = norm(A.x,A.y);
+
+    B.nombre = "B";
+    B.x = 3;
+    B.y = 5;
+    B.norma = norm(B.x,B.y);
+    
+    if (A.norma > B.norma){
+        printf("A mas lejos");
+    }
+    else{
+        printf("B mas lejos");
+    }
+
+    return 0;
+}
+
+double norm(int x , int y){
+    return(sqrt());
+}
