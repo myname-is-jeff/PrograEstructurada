f27f63e68b6cf2db61c9bc918a3afac827f43d37
myname-is-jeff <58615693+myname-is-jeff@users.noreply.github.com>
2023-04-21 16:34:35 -0300

    end G2P8 P10

---
+++ Guia2/G2P10.c
@@ -0,0 +1,30 @@
+/*
+Realizar una función que efectúe el SHIFT A DERECHA completando con 0 desde la
+izquierda. Aplicarla en un programa de ejemplo.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MASK1 2147483648
+#define MASK2 1
+
+int aBinario(unsigned int c);
+
+int main(void){
+	unsigned int c = 455;
+	printf("cantidad de Bits en 1: %d",aBinario(c));
+	return 0;
+}
+	
+int aBinario(unsigned int c){
+	int i,j;
+	j = 0;
+	for(i = 0; i < 32;i++){
+		if((((c << i) & MASK1)>> (31))&MASK2 == MASK2){
+			j++;
+		}
+		printf("%d",(((c << i) & MASK1)>> 31)&MASK2);
+	}
+	printf("\n");
+	return j;
+}
---
+++ Guia2/G2P11.c
@@ -0,0 +1,3 @@
+/*
+Realizar una función que reciba como parámetros 4 unsigned char y que coloque cada
+uno de ellos en una variable de tipo unsigned int, que devolverá en su valor de retorno.*/
