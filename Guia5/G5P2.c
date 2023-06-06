5f3f32b7b883e62a24867838ddba7ce4917242e9
myname-is-jeff <58615693+myname-is-jeff@users.noreply.github.com>
2023-06-06 12:41:48 -0300

    init G2P2

--- Guia5/G5P2.c
+++ Guia5/G5P2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct num_s{
 	int num;
@@ -7,43 +9,124 @@ struct num_s{
 
 typedef struct num_s * num_t;
 
-/* si yo le paso la lista, y agrego un elemento en el comienzo de la misma, como puedo ubicar a ese elemnto?
-esta bien que haya puesto que oAdd devuealva un puntero al primer elemento?
-*/
+typedef enum{
+	APPEND,
+	ORDER,
+	PRECITION,
+	FIRST,
+	EXIT
+}mode_t;
 
 void append(num_t lst, int val);
 void add(num_t lst, int val);
 void pAdd(num_t lst, int val, int pos);
 num_t oAdd(num_t lst, int val);
 void printLst(num_t lst);
+void loadLst(num_t * lst,char * fn);
+void addMenu(num_t * lst);
 
 int main(void){
-	struct num_s a,b,c,d,e;
 	num_t init;
-	a.num = 2;b.num = 4;c.num = 3;d.num = 8;e.num = 10;
-	a.next = &b;b.next = &c;c.next = &d;d.next = &e;e.next = NULL;
-	init = oAdd();
+	loadLst(&init,"ListNum.txt");
+	printf("%-20s%-20s%-20s\n","Valor","Dir Nodo","Dir Next");
+	printf("%060d\n",0);
+	printLst(init->next);
+	oAdd(init->next,2);
+	printf("\n%-20s%-20s%-20s\n","Valor","Dir Nodo","Dir Next");
+	printf("%060d",0);
+	printLst(init->next);
+	add(init,0);
+	printf("\n%-20s%-20s%-20s\n","Valor","Dir Nodo","Dir Next");
+	printf("%060d",0);
+	printLst(init->next);
+	
 	return 0;
 }
 
-num_t oAdd(num_t * lst, int val){
-	num_t aux;
-	if(lst != NULL){
-		if((aux = (num_t)malloc(sizeof(struct num_s))) == NULL){
-			printf("MALLOC ERROR\n");
+void loadLst(num_t * lst,char * fn){
+	FILE * fp;
+	char c[50], *p;
+	int aux;
+	if((fp = fopen(fn,"r")) == NULL){
+		printf("ERROR OPENING FILE LOADLST\n");
+	}
+	else{
+		if(((*lst) = (num_t)malloc(sizeof(struct num_s))) == NULL){
+			printf("ERROR INIT MEMORY LOADLST\n");
 		}
 		else{
-			if((lst->num) < val){
-				
+			(*lst)->next = NULL;
+			while((fgets(c,50,fp)) != NULL){
+					if((p = strchr(c,'\n')) != NULL)
+						*p = '\0';
+					aux = atoi(c);
+					append(*lst,aux);
+			}	
+		}
+		
+	}
+	fclose(fp);
+}
+
+ num_t oAdd(num_t lst, int val){
+	num_t res,aux;
+	res = NULL;
+	aux = NULL;
+	if(lst != NULL){
+		if((lst->num) > val){
+			if((res = (num_t)malloc(sizeof(struct num_s))) == NULL){
+				printf("MALLOC ERROR\n");
+			}
+			else{
+				res->num = val;
+				res->next = lst;
 			}
 		}
+		else{
+			aux = oAdd(lst->next,val);
+			if(aux != NULL){
+				lst->next = aux;
+			}
+		}
+	}
+	return res;
+}
 
+void append(num_t lst, int val){
+	num_t aux;
+	
+	if(lst != NULL){
+		if((lst->next) == NULL){
+			if((aux = (num_t)malloc(sizeof(struct num_s))) == NULL){
+				printf("ERROR INIT NEW NODE MEMORY APPEND\n");
+			}
+			else{
+				aux->num = val;
+				aux->next = NULL;
+				lst->next = aux;
+			}
+		}
+		else{
+			append(lst->next,val);
+		}
 	}
 }
 
+void add(num_t lst, int val){
+	num_t aux = NULL;
+	if((aux = (num_t)malloc(sizeof(struct num_s))) == NULL){
+		printf("ERROR INIT NEW NODE MEMORY ADD\n");
+	}
+	else{
+		aux->num = val;
+		aux->next = lst->next;
+		lst->next = aux;
+	}
+}
+	
 void printLst(num_t lst){
 	if(lst != NULL){
-		printf("\n Valor=%4d, Dir. Nodo:<%p>, dirSig:<%p>",lst->num,lst,lst->next);
+		printf("\n%-20d%-20p%-20p",lst->num,lst,lst->next);
 		printLst(lst->next);
 	}
 }
