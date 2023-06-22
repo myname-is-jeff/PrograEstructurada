ce75c9b7c6d2c49997c3f75bdcf905a8c513376f
myname-is-jeff <58615693+myname-is-jeff@users.noreply.github.com>
2023-06-22 09:51:22 -0300

    mod tp fiuba

--- TP Fiuba/TP.c
+++ TP Fiuba/TP.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 typedef enum{
-	TRUE,
-	FALSE
+	FALSE,
+	TRUE
 }bool_t;
 
 typedef enum{
@@ -32,40 +32,76 @@ struct node_s{
 
 typedef struct node_s * node_t;
 
-bool_t csv_parser(char * str, transaction_t * aux); /*paso del la linea de csv a transaction_t*/
-time_t tm_parser(char * date); /*paso el tiempo de string a time_t*/
-void tda_file_print();/*escribo todas mis transacciones ordenadas por usuario en mi archivo*/
-void tda_order_append(); /*append en orden en mi array de listas*/
+bool_t worthParse(char * str, int init, int end);/* verifico si es conveniente parsear toda la linea del archivo --- DONE*/
+void csv_parser(char * str, transaction_t * aux); /*paso del la linea de csv a transaction_t --- DONE*/
+time_t tm_parser(char * date); /*paso el tiempo de string a time_t --- DONE*/
+bool_t timeCheck(int init, int end, int time); /*Verifico que el tiempo parseado este en mi rango --- DONE*/
+void tda_file_print();/*escribo todas mis transacciones ordenadas por usuario en mi archivo --- LOADING*/
+void tda_order_append(); /*append en orden en mi array de listas --- LOADING*/
 
 int main(void){
     char str[] = "123412,1,05/11/2011 10:00:00,-10,4916288217067475,Compra supermercado\n";
     transaction_t tran;
-
-    csv_parser(str, &tran);
-    printf("USR: %d\n",tran.usr);
-    printf("TIME: %ld\n", tran.time);
-    printf("CASH: %d\n", tran.cash);
-    printf("DESCRIPTION: %s\n", tran.des);
-    printf("CARD: %s\n", tran.card);
-    printf("TRANSACTION: %d\n", tran.tran);
+	time_t init, end;
+	char i[] = "05/11/2011 10:00:00";
+	char e[] = "07/11/2011 10:00:00";
+	init = tm_parser(i); end = tm_parser(e);
+    if(worthParse(str,init,end) == TRUE){
+		csv_parser(str, &tran);
+		printf("USR: %d\n",tran.usr);
+		printf("TIME: %d\n", tran.time);
+		printf("CASH: %d\n", tran.cash);
+		printf("DESCRIPTION: %s\n", tran.des);
+		printf("CARD: %s\n", tran.card);
+		printf("TRANSACTION: %d\n", tran.tran);
+	}
+	else{
+		printf("NOT WORTH PARSING\n");
+	}
 
     return 0;
+}
+
+bool_t worthParse(char * str, int init, int end){
+	int i,j,cont = 0;
+	bool_t res;
+	char token[50];
+	
+	for(i = 0; cont != 2; i++){
+		if(str[i] == ','){
+			cont++;
+		}
+	}
+	for(j = 0; (str[i] != ',') && (str[i] != '\n');j++,i++){
+		token[j] = str[i];
+	}
+	token[j] = '\0';
+	i = tm_parser(token);
+	res = timeCheck(init,end,i);
+	return res;
+}
+
+bool_t timeCheck(int init, int end, int time){
+	bool_t flag = TRUE;
+	if((time < init) || (time > end)){
+		flag = FALSE;
+	}
+	return flag;
 }
 
 time_t tm_parser(char * date){
     struct tm aux;
     int i, j,res, cont = 0;
     char token[3];
-
-    printf("HOLA\n");
     aux.tm_isdst = 0;
     for(i = 0; date[i] != '\0';i++,cont++){
         for(j = 0; (date[i] != '\0') && (date[i] != '.') && (date[i] != '/') && (date[i] != '-') && (date[i] != ' ') && (date[i] != ':'); j++,i++){
             token[j] = date[i];
         }
-
+		if(date[i] == '\0'){
+			i--;
+		}
         token[j] = '\0';
-        printf("TM TOKEN: %s\n",token);
         switch(cont){
             case 0:
                 aux.tm_mday = atoi(token);
@@ -89,25 +125,24 @@ time_t tm_parser(char * date){
                 break;
         }
     }
-    printf("HOLA2\n");
     res = mktime(&aux);
-    printf("RES: %d", res);
     return res;
 }
 
-bool_t csv_parser(char * str, transaction_t * aux){
+void csv_parser(char * str, transaction_t * aux){
 	int i, j, cont = 0;
     char token[50];
     char * strAux;
-    bool_t ban = TRUE;
 
     if(str != NULL){
-        for(i = 0; (str[i] != '\n') && (ban == TRUE);i++,cont++){
+        for(i = 0; (str[i] != '\n');i++,cont++){
             for(j = 0; (str[i] != ',') && (str[i] != '\n');j++,i++){
                 token[j] = str[i];
             }
+			if(str[i] == '\n'){
+				i--;
+			}
             token[j] = '\0';
-            printf("TOKEN: %s\n",token);
             switch(cont){
                 case 0:
                     aux->tran = (uint)atoi(token);
@@ -119,7 +154,6 @@ bool_t csv_parser(char * str, transaction_t * aux){
                     aux->time = tm_parser(token);
                     break;
                 case 3:
-                    printf("HOLA2\n");
                     aux->cash = atoi(token);
                     break;
                 case 4:
@@ -146,5 +180,4 @@ bool_t csv_parser(char * str, transaction_t * aux){
             }
         }
     }
-    return ban;
 }
